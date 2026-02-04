2ac4f51816532197c4c3e87b7c81f1680148b4dd
Patrick Zulian <patrick.zulian@gmail.com>
2026-02-04 12:55:23 +0100

    ...

--- serial/graph/layers.cpp
+++ serial/graph/layers.cpp
@@ -6,12 +6,11 @@
 namespace ssdf {
     template <typename I, typename COUNT, typename ELINDEX>
     int create_n2e(const ptrdiff_t nelements,
-                  const ptrdiff_t nnodes,
-                  const int nnodesxelem,
-                  I **const elems,
-                  COUNT **out_n2eptr,
-                  ELINDEX **out_elindex) {
-
+                   const ptrdiff_t nnodes,
+                   const int nnodesxelem,
+                   I **const elems,
+                   COUNT **out_n2eptr,
+                   ELINDEX **out_elindex) {
         COUNT *n2eptr = (COUNT *)malloc((nnodes + 1) * sizeof(COUNT));
         memset(n2eptr, 0, (nnodes + 1) * sizeof(COUNT));
 
@@ -36,9 +35,7 @@ namespace ssdf {
         for (int edof_i = 0; edof_i < nnodesxelem; ++edof_i) {
             for (ptrdiff_t i = 0; i < nelements; ++i) {
                 ELINDEX node = elems[edof_i][i];
-
                 assert(n2eptr[node] + book_keeping[node] < n2eptr[node + 1]);
-
                 elindex[n2eptr[node] + book_keeping[node]++] = i;
             }
         }
@@ -52,27 +49,75 @@ namespace ssdf {
     }
 
     template <typename I>
+    int layers_iterative(const int nxe,
+                         I **const SSDF_RESTRICT elements,
+                         uint8_t *const SSDF_RESTRICT layers,
+                         const int max_layers,
+                         //  Auxiliary arrays
+                         ptrdiff_t *const SSDF_RESTRICT n2eptr,
+                         I *const SSDF_RESTRICT elindex,
+                         int *inout_cursor,
+                         int *inout_queue_size,
+                         int queue_capacity,
+                         I *const SSDF_RESTRICT queue) {
+        int cursor = *inout_cursor;
+        int queue_size = *inout_queue_size;
+
+        while (cursor < queue_size) {
+            I current = queue[(cursor++) % queue_capacity];
+            I ebegin = n2eptr[current];
+            I eend = n2eptr[current + 1];
+
+            for (I k = ebegin; k < eend; ++k) {
+                I e = elindex[k];
+
+                uint8_t min_layer = 255;
+                for (int i = 0; i < nxe; ++i) {
+                    I neighbor = elements[i][e];
+                    min_layer = std::min(min_layer, layers[neighbor]);
+                }
+
+                if (min_layer + 1 == max_layers) {
+                    continue;
+                }
+
+                for (int i = 0; i < nxe; ++i) {
+                    I neighbor = elements[i][e];
+
+                    if (layers[neighbor] <= min_layer + 1) {
+                        continue;
+                    }
+
+                    queue[(queue_size++) % queue_capacity] = neighbor;
+                    layers[neighbor] = min_layer + 1;
+                }
+            }
+        }
+
+        // Return the new cursor and queue size
+        *inout_cursor = cursor;
+        *inout_queue_size = queue_size;
+        return 0;
+    }
+
+    template <typename I>
     int layers(const ptrdiff_t nelements,
                const int nxe,
                I **const SSDF_RESTRICT elements,
                const ptrdiff_t npoints,
                uint8_t *const SSDF_RESTRICT layers,
                const int max_layers)
-
     {
         ptrdiff_t *n2eptr = nullptr;
         I *elindex = nullptr;
         create_n2e<I, ptrdiff_t, I>(nelements, npoints, nxe, elements, &n2eptr, &elindex);
 
-        uint8_t *visited = (uint8_t *)malloc(npoints * sizeof(uint8_t));
-        memset(visited, 0, npoints * sizeof(uint8_t));
-        I * queue = (I *)malloc(npoints * sizeof(I));
+        I *queue = (I *)malloc(npoints * sizeof(I));
         int queue_size = 0;
 
         // Multi-source BFS initialization: every node with layer==0 is a seed.
         for (ptrdiff_t i = 0; i < npoints; ++i) {
             if (layers[i] == 0) {
-                visited[i] = 1;
                 queue[queue_size++] = static_cast<I>(i);
             } else {
                 // Make sure that the point is not assigned to a layer
@@ -82,7 +127,6 @@ namespace ssdf {
 
         if (queue_size == 0) {
             fprintf(stderr, "No seed found for layers (expect at least one node with layer==0)\n");
-            free(visited);
             free(queue);
             free(n2eptr);
             free(elindex);
@@ -91,38 +135,8 @@ namespace ssdf {
 
         int cursor = 0;
 
-        while(cursor < queue_size) {
-            I current = queue[cursor++];            
-            I ebegin = n2eptr[current];
-            I eend = n2eptr[current + 1];
-            for(I k = ebegin; k < eend; ++k) {
-                I e = elindex[k];
+        layers_iterative(nxe, elements, layers, max_layers, n2eptr, elindex, &cursor, &queue_size, npoints, queue);
 
-                uint8_t min_layer = 255;
-                for(int i = 0; i < nxe; ++i) {
-                    I neighbor = elements[i][e];
-                    min_layer = std::min(min_layer, layers[neighbor]);
-                }
-
-                if(min_layer + 1 == max_layers) {
-                    continue;
-                }
-
-                for(int i = 0; i < nxe; ++i) {
-                    I neighbor = elements[i][e];
-                    if(!visited[neighbor]) {
-                        visited[neighbor] = 1;
-                        queue[queue_size++] = neighbor;
-
-                        if(layers[neighbor] == 255) {
-                            layers[neighbor] = min_layer + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        free(visited);
         free(queue);
         free(n2eptr);
         free(elindex);
@@ -130,11 +144,11 @@ namespace ssdf {
     }
 
     template int create_n2e<int, ptrdiff_t, int>(const ptrdiff_t nelements,
-                                         const ptrdiff_t nnodes,
-                                         const int nnodesxelem,
-                                         int **const elems,
-                                         ptrdiff_t **out_n2eptr,
-                                         int **out_elindex);
+                                                 const ptrdiff_t nnodes,
+                                                 const int nnodesxelem,
+                                                 int **const elems,
+                                                 ptrdiff_t **out_n2eptr,
+                                                 int **out_elindex);
 
     template int layers<int>(const ptrdiff_t nelements,
                              const int nxe,
@@ -142,4 +156,15 @@ namespace ssdf {
                              const ptrdiff_t npoints,
                              uint8_t *const SSDF_RESTRICT layers,
                              const int max_layers);
+
+    template int layers_iterative<int>(const int nxe,
+                                       int **const SSDF_RESTRICT elements,
+                                       uint8_t *const SSDF_RESTRICT layers,
+                                       const int max_layers,
+                                       ptrdiff_t *const SSDF_RESTRICT n2eptr,
+                                       int *const SSDF_RESTRICT elindex,
+                                       int *inout_cursor,
+                                       int *inout_queue_size,
+                                       int queue_capacity,
+                                       int *const SSDF_RESTRICT queue);
 }  // namespace ssdf
--- serial/graph/layers.hpp
+++ serial/graph/layers.hpp
@@ -10,16 +10,39 @@ namespace ssdf {
      * @param nxe Number of nodes per element.
      * @param elements Arrays of element indices.
      * @param npoints Number of points.
-     * @param layers Input/Output array of layer indices. Inner layer is 0, outer layer is 1. Outer layer are marked by incremeneted values.
-     * @param max_layers Maximum number of layers. If -1, the maximum number of layers is 255, 255 is used for unassigned layers.
+     * @param layers Input/Output array of layer indices. Inner layer is 0, outer layer is 1. Outer layer are marked by
+     * incremeneted values.
+     * @param max_layers Maximum number of layers. If -1, the maximum number of layers is 255, 255 is used for
+     * unassigned layers.
      */
     template <typename I>
     int layers(const ptrdiff_t nelements,
                const int nxe,
-               I** const SSDF_RESTRICT elements,
+               I **const SSDF_RESTRICT elements,
                const ptrdiff_t npoints,
-               uint8_t* const SSDF_RESTRICT layers,
+               uint8_t *const SSDF_RESTRICT layers,
                const int max_layers = 255);
+
+    template <typename I, typename COUNT, typename ELINDEX>
+    int create_n2e(const ptrdiff_t nelements,
+                   const ptrdiff_t nnodes,
+                   const int nnodesxelem,
+                   I **const elems,
+                   COUNT **out_n2eptr,
+                   ELINDEX **out_elindex);
+
+    template <typename I>
+    int layers_iterative(const int nxe,
+                         I **const SSDF_RESTRICT elements,
+                         uint8_t *const SSDF_RESTRICT layers,
+                         const int max_layers,
+                         //  Auxiliary arrays
+                         ptrdiff_t *const SSDF_RESTRICT n2eptr,
+                         I *const SSDF_RESTRICT elindex,
+                         int *inout_cursor,
+                         int *inout_queue_size,
+                         int queue_capacity,
+                         I *const SSDF_RESTRICT queue);
 }  // namespace ssdf
 
 #endif
