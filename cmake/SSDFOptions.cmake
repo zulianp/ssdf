1076a9b054106b95f802785fa426769f05ba2e2f
Patrick Zulian <patrick.zulian@gmail.com>
2026-03-11 12:03:14 +0100

    revisited cmake a little

---
+++ cmake/SSDFOptions.cmake
@@ -0,0 +1,15 @@
+include_guard(GLOBAL)
+
+# Set C++ standard
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# Build options
+option(BUILD_SHARED_LIBS "Build shared library" ON)
+option(SSDF_ENABLE_MPI "Enable MPI support" OFF)
+option(SSDF_ENABLE_OPENMP "Enable OpenMP support" OFF)
+option(SSDF_ENABLE_CUDA "Enable CUDA support" OFF)
+option(SSDF_ENABLE_HIP "Enable HIP support" OFF)
+option(SSDF_ENABLE_CUBIQL "Enable cuBQL support" OFF)
+option(SSDF_ENABLE_SMESH "Enable smesh support" ON)
---
+++ demo/mesh_distance_to_boundary.exe.cpp
@@ -0,0 +1,10 @@
+#include "smesh_mesh.hpp"
+#include "smesh_context.hpp"
+// #error "Not implemented"
+
+int main(int argc, char **argv) {
+    auto ctx = smesh::initialize(argc, argv);
+
+
+    return 0;
+}
