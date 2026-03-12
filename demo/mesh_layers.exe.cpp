518469336e6f1fdcb21835012cb3b8af6c61bcea
Patrick Zulian <patrick.zulian@gmail.com>
2026-03-12 14:02:36 +0100

    mesh_layers compute sdf

--- demo/mesh_layers.exe.cpp
+++ demo/mesh_layers.exe.cpp
@@ -1,11 +1,11 @@
 #include "smesh_buffer.hpp"
 #include "smesh_context.hpp"
 #include "smesh_env.hpp"
+#include "smesh_exchange.hpp"
 #include "smesh_mesh.hpp"
 #include "smesh_output.hpp"
 #include "smesh_path.hpp"
 #include "smesh_tracer.hpp"
-#include "smesh_exchange.hpp"
 
 #include "graph/layers.hpp"
 
@@ -17,7 +17,7 @@
 
 int main(int argc, char **argv) {
     auto ctx = smesh::initialize(argc, argv);
-    SMESH_TRACE_SCOPE("mesh_distance_to_boundary.exe");
+    SMESH_TRACE_SCOPE("mesh_layers.exe");
 
     {
         auto comm = ctx->communicator();
@@ -35,7 +35,8 @@ int main(int argc, char **argv) {
         auto surface = smesh::Mesh::create_from_file(smesh::Communicator::self(), smesh::Path(argv[2]));
 
         const ptrdiff_t n_nodes = mesh->n_nodes();
-        auto out = smesh::create_host_buffer<uint8_t>(n_nodes);
+        auto layer = smesh::create_host_buffer<uint8_t>(n_nodes);
+        auto b_layer = layer->data();
 
         const uint8_t SSDF_MAX_LAYERS = smesh::Env::read("SSDF_MAX_LAYERS", 256);
 
@@ -55,41 +56,173 @@ int main(int argc, char **argv) {
                                 sp[0],
                                 sp[1],
                                 sp[2],
-                                out->data());
-
-        auto b_out = out->data();
-        for (ptrdiff_t i = 0; i < n_nodes; i++) {
-            b_out[i] = !b_out[i];
-        }
+                                b_layer);
 
 #ifdef SMESH_ENABLE_MPI
-        {
-            ssdf::layers(mesh->n_elements(0),
-                         mesh->n_nodes_per_element(0),
-                         mesh->elements(0)->data(),
-                         mesh->n_nodes(),
-                         b_out,
-                         SSDF_MAX_LAYERS);
+        if (comm->size() > 1) {
+            auto dist = mesh->distributed();
+            auto exchange = smesh::Exchange::create_nodal(mesh, smesh::Exchange::ExchangeScope::GhostsAndAura);
 
-            auto exchange = smesh::Exchange::create_nodal(mesh, smesh::Exchange::ExchangeScope::GhostsOnly);
-            exchange->gather(b_out);
+            ptrdiff_t *n2e_ptr{nullptr};
+            smesh::idx_t *n2e_idx{nullptr};
+
+            ssdf::create_n2e(mesh->n_elements(0),
+                             mesh->n_nodes(),
+                             mesh->n_nodes_per_element(0),
+                             mesh->elements(0)->data(),
+                             &n2e_ptr,
+                             &n2e_idx);
+
+            auto queue = smesh::create_host_buffer<smesh::idx_t>(n_nodes);
+            auto b_queue = queue->data();
+
+            int queue_size = 0;
+            for (ptrdiff_t i = 0; i < n_nodes; ++i) {
+                if (b_layer[i] == 1) {
+                    b_layer[i] = 0;
+                    b_queue[queue_size++] = static_cast<smesh::idx_t>(i);
+                } else {
+                    // Make sure that the point is not assigned to a layer
+                    b_layer[i] = 255;
+                }
+            }
+
+            const ptrdiff_t n_changing = n_nodes - dist->n_nodes_owned();
+            auto changing = smesh::create_host_buffer<uint8_t>(n_changing);
+            auto b_changing = changing->data();
+            memcpy(b_changing, &b_layer[dist->n_nodes_owned()], sizeof(*b_layer) * n_changing);
+
+            for (int i = 0; i < comm->size() * 1000; i++) {
+                int cursor = 0;
+                ssdf::layers_iterative(mesh->n_nodes_per_element(0),
+                                       mesh->elements(0)->data(),
+                                       b_layer,
+                                       SSDF_MAX_LAYERS,
+                                       //  Auxiliary arrays
+                                       n2e_ptr,
+                                       n2e_idx,
+                                       &cursor,
+                                       &queue_size,
+                                       queue->size(),
+                                       queue->data());
+
+                exchange->gather(b_layer);
+
+                const ptrdiff_t start = dist->n_nodes_owned();
+                const ptrdiff_t end = n_nodes;
+                ptrdiff_t n_changes = 0;
+                for (ptrdiff_t i = start; i < end; i++) {
+                    n_changes += b_layer[i] != b_changing[i - start];
+                    b_changing[i - start] = b_layer[i];
+                }
+
+                n_changes = comm->sum(n_changes);
+                if (!n_changes) {
+                    if (!comm->rank()) {
+                        printf("Finished at iteration %i\n", i);
+                    }
+                    break;
+                }
+
+                queue_size = 0;
+
+                for (ptrdiff_t i = start; i < end; i++) {
+                    b_queue[queue_size++] = i;
+                }
+            }
+
+            free(n2e_ptr);
+            free(n2e_idx);
 
             // Repeat layers and see if something changed, alltoall to synch
-        }
-#else
-
+        } else
+#endif
         {
+
+            for (ptrdiff_t i = 0; i < n_nodes; ++i) {
+                b_layer[i] = b_layer[i] != 1;
+            }
+
             ssdf::layers(mesh->n_elements(0),
                          mesh->n_nodes_per_element(0),
                          mesh->elements(0)->data(),
                          mesh->n_nodes(),
-                         out->data(),
+                         b_layer,
                          SSDF_MAX_LAYERS);
         }
-#endif
 
         auto output = smesh::Output::create(mesh, smesh::Path(argv[3]));
-        output->write_nodal("layer", smesh::astype<float>(out));
+        output->write_nodal("layer", smesh::astype<float>(layer));
+
+        {
+            auto sdf = smesh::create_host_buffer<smesh::geom_t>(n_nodes);
+            auto b_sdf = sdf->data();
+#pragma omp parallel for
+            for (ptrdiff_t i = 0; i < n_nodes; i++) {
+                b_sdf[i] = std::numeric_limits<smesh::geom_t>::max();
+            }
+
+            {
+                auto boundary = smesh::skin(mesh);
+                auto be = boundary->elements(0)->data();
+                auto bp = boundary->points()->data();
+
+#ifdef SMESH_ENABLE_MPI
+                auto dist = mesh->distributed();
+                ssdf::pedf(comm->get(),
+                           dist->n_nodes_owned(),
+                           p[0],
+                           p[1],
+                           p[2],
+                           boundary->n_elements(),
+                           be[0],
+                           be[1],
+                           be[2],
+                           boundary->n_nodes(),
+                           bp[0],
+                           bp[1],
+                           bp[2],
+                           b_sdf);
+#else
+                ssdf::edf_select(mesh->n_nodes(),
+                                 p[0],
+                                 p[1],
+                                 p[2],
+                                 boundary->n_elements(),
+                                 be[0],
+                                 be[1],
+                                 be[2],
+                                 boundary->n_nodes(),
+                                 bp[0],
+                                 bp[1],
+                                 bp[2],
+                                 b_sdf);
+#endif
+            }
+
+            ssdf::edf_select(mesh->n_nodes(),
+                             p[0],
+                             p[1],
+                             p[2],
+                             surface->n_elements(),
+                             selements[0],
+                             selements[1],
+                             selements[2],
+                             surface->n_nodes(),
+                             sp[0],
+                             sp[1],
+                             sp[2],
+                             b_sdf);
+
+#pragma omp parallel for
+            for (ptrdiff_t i = 0; i < n_nodes; i++) {
+                if (b_layer[i] == 0) {
+                    b_sdf[i] = -b_sdf[i];
+                }
+            }
+
+            output->write_nodal("sdf", sdf);
+        }
     }
 
     return 0;
